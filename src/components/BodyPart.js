860cef12b451374501a9150d86ea03d76eb1b099
Deb Kalyan <Deb.Kalyan@markoknow.com>
2023-12-24 23:36:26 +0530

    Added The HeroBanner

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Stack , Typography } from '@mui/material';
-
+import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = ({item , setBodyPart  , bodyPart}) => {
-  return (
-    <Stack
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  <Stack
     type="button"
     alignItems="center"
     justifyContent="center"
@@ -19,7 +17,6 @@ const BodyPart = ({item , setBodyPart  , bodyPart}) => {
     <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
     <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
   </Stack>
-  )
-}
+);
 
-export default BodyPart
+export default BodyPart;
